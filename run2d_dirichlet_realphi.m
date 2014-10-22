7d16d06a49bf707e8f1b61eca7ddc14d109f51fb
Jon Christian Halvorsen <jchalvorsen@gmail.com>
2014-10-22 09:58:28 +0200

    Added 'simple mode'

--- run2d_dirichlet_realphi.m
+++ run2d_dirichlet_realphi.m
@@ -14,42 +14,41 @@ u = @(x) sin(2*pi*(x(1)^2+x(2)^2));
 A = sparse(N,N);
 b = zeros(N,1);
 
-divpsi1 = [-1;-1];
-divpsi2 = [1; 0];
-divpsi3 = [0; 1];
-divpsi = [divpsi1, divpsi2, divpsi3];
-
 for i = 1:length(tri)
     nodes = tri(i,:);
     P = p(nodes,:);       % Active points in triangle
     
-    % Finding basis function:
-    phi1 = @(x) ([1, x(1), x(2)]*(Q\[1; 0; 0]))*f(x(1),x(2));
-    phi2 = @(x) ([1, x(1), x(2)]*(Q\[0; 1; 0]))*f(x(1),x(2));
-    phi3 = @(x) ([1, x(1), x(2)]*(Q\[0; 0; 1]))*f(x(1),x(2));
-    
-    %% Getting stiffness matrix
     % Calculating area
     Q = [[1;1;1], P];
     area = 0.5*abs(det(Q));
     
-    % Constructing jacobian and solving shit
-    Jac = [P(2,:) - P(1,:); P(3,:) - P(1,:)];
-   
+    %% Getting stiffness matrix 
+    % Finding constants in phi (basis function = [1, x, y] * c)
+    c1 = Q\[1; 0; 0];
+    c2 = Q\[0; 1; 0];
+    c3 = Q\[0; 0; 1];
     
-    % Ak is element matrix for this element
-    Ak = transpose(Jac\divpsi)*(Jac\divpsi)*area;
+    divphi1 = c1(2:3);
+    divphi2 = c2(2:3);
+    divphi3 = c3(2:3);
+    divphi = [divphi1, divphi2, divphi3];
     
+    % Ak is element matrix for this element 
+    Ak = divphi'*divphi*area;
+      
     % Put element matrix in right place
     A(nodes,nodes) = A(nodes,nodes) + Ak;
     
     %% Getting b vector 
-
+    % Finding functions phi*f:
+    f1 = @(x) ([1, x(1), x(2)]*c1)*f(x(1),x(2));
+    f2 = @(x) ([1, x(1), x(2)]*c2)*f(x(1),x(2));
+    f3 = @(x) ([1, x(1), x(2)]*c3)*f(x(1),x(2));
     
     % Getting values:
-    val1 = quadrature2d(P(1,:), P(2,:), P(3,:), 4, phi1);
-    val2 = quadrature2d(P(1,:), P(2,:), P(3,:), 4, phi2);
-    val3 = quadrature2d(P(1,:), P(2,:), P(3,:), 4, phi3);
+    val1 = quadrature2d(P(1,:), P(2,:), P(3,:), 4, f1);
+    val2 = quadrature2d(P(1,:), P(2,:), P(3,:), 4, f2);
+    val3 = quadrature2d(P(1,:), P(2,:), P(3,:), 4, f3);
     
     % Putting in right place:
     b(nodes) = b(nodes) + [val1; val2; val3];  
@@ -85,16 +84,16 @@ for i = 1:length(tri)
     % Finding basis function:
     Q = [[1;1;1], P];
     phi1 = @(x) [1, x(1), x(2)]*(Q\[1; 0; 0]);
-    phi2 = @(x) [1, x(1), x(2)]*(Q\[0; 1; 0]);
-    phi3 = @(x) [1, x(1), x(2)]*(Q\[0; 0; 1]);
+    f2 = @(x) [1, x(1), x(2)]*(Q\[0; 1; 0]);
+    f3 = @(x) [1, x(1), x(2)]*(Q\[0; 0; 1]);
        
     uu = zeros(length(x), length(y));
     for j = 1:length(x)
         for k = 1:length(y)
             point = [x(j), y(k)];
             phi1v = phi1(point);
-            phi2v = phi2(point);
-            phi3v = phi3(point);
+            phi2v = f2(point);
+            phi3v = f3(point);
             q = [phi1v, phi2v, phi3v];
             % Check if point is inside the triangle
             if (phi1v <= 1 && phi1v >= 0) && (phi2v <= 1 && phi2v >= 0) && (phi3v <= 1 && phi3v >= 0)
