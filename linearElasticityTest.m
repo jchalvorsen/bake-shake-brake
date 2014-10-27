0213c22bf05026018fdfb22f3edf7dbde96e5b2c
Jon Christian Halvorsen <jchalvorsen@gmail.com>
2014-10-27 15:58:09 +0100

    Small fixes

--- linearElasticityTest.m
+++ linearElasticityTest.m
@@ -47,17 +47,17 @@ clear all
 
 addpath include
 
-N = 40;
+N = 20;
 
 [p tri edge] = getPlate(N);
-figure
-triplot(tri, p(:,1), p(:,2))
+%figure
+%triplot(tri, p(:,1), p(:,2))
 
 N = length(p);
 
 % Declaring material constants
-E = 0.1;
-v = 0.01;
+E = 1;
+v = 0.1;
 
 % Building the C matrix:
 C = E/(1-v^2)*[1, v, 0; v, 1, 0; 0, 0, 1-v];
@@ -85,14 +85,14 @@ for i = 1:length(tri)
     c1 = Q\[1; 0; 0];
     c2 = Q\[0; 1; 0];
     c3 = Q\[0; 0; 1];
-    
+    %c = inv(Q);
     c = [c1, c2, c3];
     
     for j = 0:1     % looping over x and y indexing
         Ak = zeros(3);
         for q = 1:3
             for w = 1:3
-
+                    % Basis function = c1 + c2*x + c3*y
                     %e = [c2; 0; 0.5*c3];
                     e1x = [c(2,q); 0; 0.5*c(3,q)];
                     e2x = [c(2,w); 0; 0.5*c(3,w)];
@@ -100,7 +100,7 @@ for i = 1:length(tri)
                     %ey = [0; c(3); 0.5*c(2)];
                     e1y = [0; c(3,q); 0.5*c(2,q)];
                     e2y = [0; c(3,w); 0.5*c(2,w)];
-            
+
                 if j == 0
                     f = e1x'*C*e2x;
                 else 
@@ -110,6 +110,7 @@ for i = 1:length(tri)
                 Ak(q,w) = Ak(q,w) + f*area;
             end
         end
+        %Ak
         % Put element matrix in right place      
         
         A(2*nodes-1+j,2*nodes-1+j) = A(2*nodes-1+j,2*nodes-1+j) + Ak;
@@ -138,91 +139,16 @@ end
 %% Get A without boundary points
 boundaryPoints = [2*edge'-1; 2*edge'];
 
-% Setting rows and cols of boundaryPoints equal to 0
+% Setting cols of boundaryPoints equal to 0
 A(boundaryPoints, :) = 0;
 %A(:, boundaryPoints) = 0;
 b(boundaryPoints) = 0;
-A(boundaryPoints, boundaryPoints) = speye(length(boundaryPoints), length(boundaryPoints));
+A(boundaryPoints, boundaryPoints) = A(boundaryPoints, boundaryPoints) + speye(length(boundaryPoints), length(boundaryPoints));
 
 % Solving the linear system
 u_sol = A\b;
 
-%% Finding solution on M*M grid and plotting
-M = 100;
-U_sol = zeros(M,M);
-z = linspace(-1,1,M);
-for i = 1:length(tri)
-    nodes = tri(i,:);
-    P = p(nodes,:);       % Active points in triangle
-   
-    max_x = max(P(:,1));
-    min_x = min(P(:,1));
-    max_y = max(P(:,2));
-    min_y = min(P(:,2));
-    
-    x = z(max_x >= z & z >= min_x);
-    y = z(max_y >= z & z >= min_y);
-    
-    % Finding basis function:
-    Q = [[1;1;1], P];
-    phi1 = @(x) [1, x(1), x(2)]*(Q\[1; 0; 0]);
-    phi2 = @(x) [1, x(1), x(2)]*(Q\[0; 1; 0]);
-    phi3 = @(x) [1, x(1), x(2)]*(Q\[0; 0; 1]);
-       
-    uu = zeros(length(x), length(y));
-    for j = 1:length(x)
-        for k = 1:length(y)
-            point = [x(j), y(k)];
-            phi1v = phi1(point);
-            phi2v = phi2(point);
-            phi3v = phi3(point);
-            q = [phi1v, phi2v, phi3v];
-            % Check if point is inside the triangle
-            % -------------------------------------------------------------
-            % Her er vi uheldige og finner noen punkter 2 eller 0 ganger,
-            % avhengig av om vi bruker >=0 eller >0, henholdsvis:
-            % Vurder ny plottingsmetode, trisurf andbefales av studass
-            % -------------------------------------------------------------
-            if (phi1v <= 1 && phi1v > 0) && (phi2v <= 1 && phi2v > 0) && (phi3v <= 1 && phi3v > 0)
-                % Add if inside triangle
-                
-                uu(j,k) = q*u_sol(2*nodes);
-                
-                % Question: What about u_sol(2*nodes-1)?
-            end
-     
-        end
-    end   
-    
-    % merging uu into U_sol
-    xstart = find(z == x(1));
-    xend = xstart + length(x) - 1;
-    ystart = find(z == y(1));
-    yend = ystart + length(y) - 1;
-
-    U_sol(xstart:xend, ystart:yend) = uu + U_sol(xstart:xend, ystart:yend);
-end
- 
-
-figure
-subplot(1,2,1)
-surf(z, z, U_sol)
-title('FEM solution')
-
-
-% Plotting reference solution:
-U = zeros(100);
-z = linspace(-1,1);
-for i = 1:100
-    for j = 1:100
-        pz = [z(i), z(j)];
-        U(i,j) = u(pz);
-    end
-end
-subplot(1,2,2)
-surf(z,z,U)
-title('Reference solution')
-
+%% Plotting:
 figure
 hold on
 % Scatterplot of our result points
@@ -259,6 +185,13 @@ view(2),axis equal,colorbar, title('Analytical solution')
 
 
 
+figure
+subplot(1,2,1)
+trisurf(tri, p(:,1), p(:,2), u_sol(1:2:end))
+subplot(1,2,2)
+trisurf(tri, p(:,1), p(:,2), u_ref(1:2:end))
 
+figure
+trisurf(tri, p(:,1), p(:,2), u_sol(1:2:end) - u_ref(1:2:end))
 
 
