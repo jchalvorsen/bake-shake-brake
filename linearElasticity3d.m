2ddecc506142fc4a51e7d890f1504a604700fedb
Jon Christian Halvorsen <jchalvorsen@gmail.com>
2014-10-28 15:19:53 +0100

    First try 3d

--- linearElasticity3d.m
+++ linearElasticity3d.m
@@ -14,7 +14,7 @@ tetr = load('coarse_element.m');
 p = load('coarse_nodes.m');
 
 
-zeronodes = find((p(:,3) == 0)); % Dirichlet homogenous BC: f(zeronodes) = 0
+
 
 % Write structure to vtf file
 %writeVTF(p, tetr, 0, 'queen.vtf')
@@ -24,7 +24,160 @@ zeronodes = find((p(:,3) == 0)); % Dirichlet homogenous BC: f(zeronodes) = 0
 %tetramesh(tetr, p)
 
 
+N = length(p);
 
+% Declaring material constants
+E = 1;
+v = 0.1;
+
+% Building the C matrix:
+
+C1 =  E*v/((1+v)*(1-2*v))*ones(3,3) + E/(1+v)*eye(3);
+C2 = E/(2*(1+v))*eye(3);
+C = inv([ C1        , zeros(3,3);
+      zeros(3,3), C2        ]); % pretty sure this should be inverted
+  % found at www.rpi.edu/~des/3DElasticity.ppt, slide 24 (inverse function)
+
+density = @(x,y,z) 1;
+
+A = sparse(3*N,3*N);
+b = zeros(3*N,1);
+h = waitbar(0, 'In progress');
+for i = 1:length(tetr)
+    if mod(i,1000) == 0
+        waitbar(i/length(tetr),h, 'In progress')
+    end
+    nodes = tetr(i,:);
+    P = p(nodes,:);       % Active points in tetrangle
+    
+    % Calculating area
+    Q = [[1;1;1;1], P];
+    vol = abs(det(Q))/6;
+    
+    
+    %% Getting stiffness matetrx
+    % Finding constants in phi (basis function = [1, x, y, z] * c)
+    c1 = Q\[1; 0; 0; 0];
+    c2 = Q\[0; 1; 0; 0];
+    c3 = Q\[0; 0; 1; 0];
+    c4 = Q\[0; 0; 0; 1];
+    %c = inv(Q);
+    c = [c1, c2, c3, c4];
+    
+    % looping over x and y indexing
+    Ak = zeros(12);
+    for q = 1:4
+        for w = 1:4
+            % Basis function x = [c1 + c2*x + c3*y, c4*z; 0; 0]
+            % y = [0; c1 + c2*x + c3*y + c4*z; 0]
+            % z = [0; 0; c1 + c2*x + c3*y + c4*z]
+            %e = [c2; 0; 0.5*c3];
+            
+            %{
+             The eps have form of [-; -; -; -; -; -] (1x6)
+             and have the form:
+             eps = Du where, u (1x3),
+             D = [ delx,    0,    0;
+                   0,    dely,    0;
+                   0,       0, delz;
+                   dely, delx,    0;
+                   0,    delz, dely;
+                   delz,    0, delx ]      
+            %}
+            
+            ex = @(i) [c(2,i); 0; 0; c(3,i); 0; c(4,i)];
+            ey = @(i) [0; c(3,i); 0; c(2,i); c(4,i); 0];
+            ez = @(i) [0; 0; c(4,i); 0; c(3,i); c(2,i)];
+            
+            
+            % q represents basis function #1, while w is #2         
+            f11 = ex(q)'*C*ex(w);
+            f12 = ex(q)'*C*ey(w);
+            f13 = ex(q)'*C*ez(w);
+            f22 = ey(q)'*C*ey(w);
+            f23 = ey(q)'*C*ez(w);
+            f33 = ez(q)'*C*ez(w);
+            
+            fk = [f11, f12, f13;
+                  f12, f22, f23;
+                  f13, f23, f33];
+              
+            Ak(3*q-2:3*q, 3*w-2:3*w) = fk*vol;
+        end
+    end
+    %Ak
+    % Put element matetrx in right place
+    % Map to right place:
+    map(1:3:3*length(nodes)) = 3*nodes-2;
+    map(2:3:3*length(nodes)) = 3*nodes-1;
+    map(3:3:3*length(nodes)) = 3*nodes;
+    A(map,map) = A(map,map) + Ak;
+
+    %% Getting b vector
+    % Finding functions phi*f:
+    
+    f1 = @(x) ([1, x(1), x(2), x(3)]*c1)*density(x);
+    f2 = @(x) ([1, x(1), x(2), x(3)]*c2)*density(x);
+    f3 = @(x) ([1, x(1), x(2), x(3)]*c3)*density(x);
+    f4 = @(x) ([1, x(1), x(2), x(3)]*c4)*density(x);
+    
+    % Getting values:
+    val1 = quadrature3d(P(1,:), P(2,:), P(3,:), P(4,:), 5, f1);
+    val2 = quadrature3d(P(1,:), P(2,:), P(3,:), P(4,:), 5, f2);
+    val3 = quadrature3d(P(1,:), P(2,:), P(3,:), P(4,:), 5, f3);
+    val4 = quadrature3d(P(1,:), P(2,:), P(3,:), P(4,:), 5, f4);
+    
+    % Putting in right place:
+    for j = 0:2
+        b(3*nodes-2+j) = b(3*nodes-2+j) + [val1; val2; val3; val4]*vol*-9.81;
+    end
+
+end
+%% Get A without boundary points
+boundaryPoints = find((p(:,3) == 0)); % Dirichlet homogenous BC: f(boundaryPoints) = 0
+
+% Setting cols of boundaryPoints equal to 0
+A(boundaryPoints, :) = 0;
+%A(:, boundaryPoints) = 0;
+b(boundaryPoints) = 0;
+A(boundaryPoints, boundaryPoints) = A(boundaryPoints, boundaryPoints) + speye(length(boundaryPoints), length(boundaryPoints));
+
+% Solving the linear system
+u_sol = A\b;
+
+%% Plotting:
+% figure
+% hold on
+% % Scatterplot of our result points
+% for i = 1:length(p)
+%     point = p(i,:);
+%     plot3(point(1), point(2), u_sol(i),'*')
+% end
+% title('Scatterplot of results')
+
+
+% Finding reference solution in points:
+% u_ref = zeros(2*N,1);
+% for i = 1:length(p)
+%     point = p(i,:);
+%     u_ref(2*i) = u(point);
+%     u_ref(2*i-1) =u(point);
+% end
+
+figure
+%plot(u_ref, '*-black')
+hold on
+plot(u_sol, '*-b')
+title('Values in points')
+
+
+figure
+%subplot(2,1,1)
+trisurf(tetr,p(:,1),p(:,2),p(:,3),u_sol(1:3:end));
+view(2),axis equal,colorbar,title('FEM solution')
+
+
+U = [u_sol(1:3:end), u_sol(2:3:end), u_sol(3:3:end)];
 
 
 
---
+++ writeVTF2.m
@@ -0,0 +1,222 @@
+function writeVTF2(p, tetr, varargin),
+% function writeVTF2(p, tetr, <name>, value, <name>, value, ...),
+% description:
+%    writes 3d volumetric data to Ceetron GLview native format (.vtf)
+%
+% arguments:
+%   - p        point cloud (nx3 matrix of n (x,y,z)-coordinates)
+%   - tetr     tetrahedral elements. Index to the four corners of element i given in row i.
+%   - <name>   name of result set, a descriptive string
+%   - value    values of that particular results
+%
+%  The interpretation of the result values are based on their size:
+%     n x 1    - vectors are scalar results       (ex: temperature field)
+%     n x 3    - matrices are vector results      (ex: displacement field)
+%     n x m    - matrices are scalar time results (ex: time-dependent temperature)
+%     n x 3 x m- 3D-matrices are multiple vector results (ex: eigenmodes)
+%
+%  Except for the following special <name> tags:
+%    'FileName' - name of file to store results in
+%    'Time'     - scalar values of the time iteration
+%    
+% returns:
+%   none
+%   
+% EXAMPLES:
+%   % store a scalar temperature field 
+%   load tetmesh;
+%   u = sqrt(sum(X.^2,2));
+%   writeVTF2(X, tet, 'Temperature', u, 'FileName', 'myTemp.vtf');
+%    
+%   % store a time-varying temperature field
+%   load tetmesh;
+%   t  = linspace(0,2*pi, 50);
+%   u  = sqrt(sum(X.^2,2));
+%   Ut = u * t;
+%   writeVTF2(X, tet, 'Temperature', Ut, 'Time', t, 'FileName', 'myTempTime.vtf');
+%    
+%   % store a vector displacement field
+%   load tetmesh;
+%   uz = 10*sin(2*pi*X(:,3)/max(X(:,3)));
+%   uy = sqrt(sum(X.^2,2));
+%   ux = zeros(size(uz));
+%   U  = [ux, uy, uz];
+%   writeVTF2(X, tet, 'Displacement', U, 'FileName', 'myDisplacement.vtf');
+%    
+%   % store a set of 3 eigenmodes
+%   load tetmesh;
+%   n  = size(X,1);
+%   uz = 10*sin(2*pi*X(:,3)/max(X(:,3)));
+%   uy = sqrt(sum(X.^2,2));
+%   ux = zeros(size(uz));
+%   allU = zeros(n,3,3);
+%   allU(:,:,1) = [ux, uy, uz];
+%   allU(:,:,2) = [uz, uy, ux];
+%   allU(:,:,3) = [uz, uz, uz];
+%   writeVTF2(X, tet, 'Eigenmodes', allU, 'FileName', 'myEigenmodes.vtf');
+%    
+%   % store a vector displacement field along with a scalar von mises stress
+%   load tetmesh;
+%   uz    = 10*sin(2*pi*X(:,3)/max(X(:,3)));
+%   uy    = sqrt(sum(X.^2,2));
+%   ux    = zeros(size(uz));
+%   sigma = 10*sin(4*pi*X(:,1)/max(X(:,1)));
+%   U     = [ux, uy, uz];
+%   writeVTF2(X, tet, 'Displacement', U, 'Von Mises Stress', sigma, 'FileName', 'myDisplacement.vtf');
+%
+
+% author: Kjetil A. Johannessen
+% last edit: November 2012
+
+filename = '';
+timeSteps     = [];
+
+for i=1:2:numel(varargin),
+	if(strcmp(varargin{i}, 'FileName')),
+		filename = varargin{i+1};
+	elseif(strcmp(varargin{i}, 'Time')),
+		timeSteps = varargin{i+1};
+	end
+end
+
+if(numel(timeSteps) == 0),
+	timeSteps = 1:size(p,1);
+end
+
+if strcmp(filename, ''),
+	filename = 'out.vtf';
+	disp 'writing result to "out.vtf"';
+end
+
+fid = fopen(filename, 'wt');
+fprintf(fid, '*VTF-1.00\n');
+fprintf(fid, '\n');
+fprintf(fid, '*INTERNALSTRING 40001\n');
+fprintf(fid, 'VTF Writer Version info:\n');
+fprintf(fid, 'APP_INFO: GLview Express Writer: 1.1-12\n');
+fprintf(fid, 'GLVIEW_API_VER: 2.1-22\n');
+time = clock;
+fprintf(fid, 'EXPORT_DATE: %d-%d-%d %02d:%02d:%02d\n', int32(time));
+fprintf(fid, '\n');
+
+fprintf(fid, '*NODES 1\n');
+fprintf(fid, '%f %f %f\n', p');
+fprintf(fid, '\n');
+
+fprintf(fid, '*ELEMENTS 1\n');
+fprintf(fid, '%%NODES #1\n');
+fprintf(fid, '%%NAME "Patch 1"\n');
+fprintf(fid, '%%NO_ID\n');
+fprintf(fid, '%%MAP_NODE_INDICES\n');
+fprintf(fid, '%%PART_ID \n');
+fprintf(fid, '%%TETRAHEDRONS\n');
+fprintf(fid, '%d %d %d %d\n', tetr');
+fprintf(fid, '\n');
+
+
+res_id = 2;
+for i=1:2:numel(varargin),
+	if(strcmp(varargin{i}, 'FileName') || strcmp(varargin{i}, 'Time') )
+		continue;
+	end
+	u = varargin{i+1};
+	n = size(u);
+
+	if(numel(n) > 2)  % time vector field
+		for j=1:n(2),
+			fprintf(fid, '*RESULTS %d \n', res_id);
+			fprintf(fid, '%%NO_ID \n');
+			fprintf(fid, '%%DIMENSION 3 \n');
+			fprintf(fid, '%%PER_NODE #1 \n');
+			fprintf(fid, '%f %f %f \n', u(:,:,j)');
+			fprintf(fid, '\n');
+			res_id = res_id + 1;
+		end
+	elseif(n(2) > 3) % time scalar field
+		for j=1:n(2),
+			fprintf(fid, '*RESULTS %d \n', res_id);
+			fprintf(fid, '%%NO_ID \n');
+			fprintf(fid, '%%DIMENSION 1 \n');
+			fprintf(fid, '%%PER_NODE #1 \n');
+			fprintf(fid, '%f \n', u(:,j));
+			fprintf(fid, '\n');
+			res_id = res_id + 1;
+		end
+	elseif(n(2) == 3) % vector field
+		fprintf(fid, '*RESULTS %d \n', res_id);
+		fprintf(fid, '%%NO_ID \n');
+		fprintf(fid, '%%DIMENSION 3 \n');
+		fprintf(fid, '%%PER_NODE #1 \n');
+		fprintf(fid, '%f %f %f \n', u');
+		res_id = res_id + 1;
+	elseif(n(2) == 1) % scalar field
+		fprintf(fid, '*RESULTS %d \n', res_id);
+		fprintf(fid, '%%NO_ID \n');
+		fprintf(fid, '%%DIMENSION 1 \n');
+		fprintf(fid, '%%PER_NODE #1 \n');
+		fprintf(fid, '%f \n', u);
+		res_id = res_id + 1;
+	else
+		info = '';
+		sprintf(info, 'Nonvalid dimensions (%d,%d) of solution field %s', n(1), n(2), varargin{i});
+		disp(info);
+		fclose(fid);
+		return;
+	end
+	fprintf(fid, '\n');
+end
+
+fprintf(fid, '*GLVIEWGEOMETRY 1\n');
+fprintf(fid, '%%STEP 1\n');
+fprintf(fid, '%%ELEMENTS\n');
+fprintf(fid, '1 \n');
+fprintf(fid, '\n');
+
+res_id = 2;
+for i=1:2:numel(varargin),
+	if(strcmp(varargin{i}, 'FileName') || strcmp(varargin{i}, 'Time') )
+		continue;
+	end
+	u = varargin{i+1};
+	n = size(u);
+
+	if(numel(n) > 2)  % time vector field
+		fprintf(fid, '*GLVIEWVECTOR 1\n');
+		fprintf(fid, '%%NAME "%s"\n', varargin{i});
+		for j=1:n(2),
+			fprintf(fid, '%%STEP %d\n', j);
+			fprintf(fid, '%%STEPNAME %d\n', timeSteps(j));
+			fprintf(fid, '%d \n', res_id);
+			res_id = res_id + 1;
+		end
+		fprintf(fid, '\n');
+	elseif(n(2) > 3) % time scalar field
+		fprintf(fid, '*GLVIEWSCALAR 1\n');
+		fprintf(fid, '%%NAME "%s"\n', varargin{i});
+		for j=1:n(2),
+			fprintf(fid, '%%STEP %d\n', j);
+			fprintf(fid, '%%STEPNAME %d\n', timeSteps(j));
+			fprintf(fid, '%d \n', res_id);
+			res_id = res_id + 1;
+		end
+		fprintf(fid, '\n');
+	elseif(n(2) == 3) % vector field
+		fprintf(fid, '*GLVIEWVECTOR 1\n');
+		fprintf(fid, '%%NAME "%s"\n', varargin{i});
+		fprintf(fid, '%%STEP 1\n');
+		fprintf(fid, '%d \n', res_id);
+		fprintf(fid, '\n');
+		res_id = res_id + 1;
+	elseif(n(2) == 1) % scalar field
+		fprintf(fid, '*GLVIEWSCALAR 1\n');
+		fprintf(fid, '%%NAME "%s"\n', varargin{i});
+		fprintf(fid, '%%STEP 1\n');
+		fprintf(fid, '%d \n', res_id);
+		fprintf(fid, '\n');
+		res_id = res_id + 1;
+	end
+end
+
+fclose(fid);
+
+end
