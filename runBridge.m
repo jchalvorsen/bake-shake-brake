5ea89d3d53ceeb4b06df488b17bd6eac4dc0d47e
Jon Christian Halvorsen <jchalvorsen@gmail.com>
2014-11-04 13:27:57 +0100

    Small fixes

--- runBridge.m
+++ runBridge.m
@@ -20,23 +20,29 @@ m = min(pts);
 pts = [pts(:,1) - m(1), pts(:,2) - m(2), pts(:,3) - m(3)];
 
 % skewing car to start at right spot:
-z_dir = min(pts_car(:,3));
-pts_car = [pts_car(:,1) - m(1), pts_car(:,2) - m(2), pts_car(:,3) - z_dir];
+
 
 N = max(size(pts_car)) + max(size(pts));
-u_sol = zeros(3*N,2);
 
-ts = 1;
+fig = figure;
+%%
+ts = 25;
+
+M = length(ts);
+u_sol = zeros(3*N,M);
 for t = 1:max(size(ts));
+    pts_old = pts_car;
     % Setting position of car:
     %t = 8;
-    pts_car = [pts_car(:,1:2), pts_car(:,3) + ts(t)];
+    z_dir = min(pts_car(:,3)) - ts(t);
+    pts_car_new = [pts_car(:,1) - m(1), pts_car(:,2) - m(2), pts_car(:,3) - z_dir];
+    %pts_car_new = [pts_car(:,1:2), pts_car(:,3) + ts(t)];
     
     % combining car and bridge mappings:
-    mapping = zeros(length(pts_car),1);
-    for i = 1:length(pts_car)
+    mapping = zeros(length(pts_car_new),1);
+    for i = 1:length(pts_car_new)
         for j = 1:length(pts)
-            if sum(pts_car(i,:) == pts(j,:)) == 3
+            if sum(pts_car_new(i,:) == pts(j,:)) == 3
                 mapping(i) = j;
             end
         end
@@ -52,16 +58,16 @@ for t = 1:max(size(ts));
     end
     
     % making new data with both car and bridge
-    newpoints = [pts; pts_car];
+    newpoints = [pts; pts_car_new];
     newelements = [el; el_car2];
-    %[newpoints el] = getLargestConnectedDomain(newpoints, el);
+    %[newpoints newelements] = getLargestConnectedDomain(newpoints, newelements);
     data = hex2tetr(newelements);
     tetr = data(:,1:4);
     p = [newpoints(:,1), newpoints(:,3), newpoints(:,2)]; % fixing minecraft y-z axis
     
     
     
-    
+    isequal(pts_old,pts_car)
     writeVTF(p, tetr, data(:,5), 'bridge.vtf')
     
     % We then have tetr and pts and are ready to do the analysis
@@ -81,9 +87,9 @@ for t = 1:max(size(ts));
         zeros(3,3), C2        ]; % pretty sure this should be inverted
     % found at www.rpi.edu/~des/3DElasticity.ppt, slide 24 (inverse function)
     
-    density = @(x) 7750 + (x(3) > 8)*50000; % kg/ m^3
+    density = @(x) 7750 + (x(3) > 8)*500000; % kg/ m^3
     
-    A = sparse(3*N,3*N);
+    A = zeros(3*N,3*N);
     b = zeros(3*N,1);
     h = waitbar(0, 'In progress');
     for i = 1:length(tetr)
@@ -195,6 +201,15 @@ for t = 1:max(size(ts));
     % Solving the linear system
     u_sol(:,t) = Asp\b;
     
+    % Plotting inside loop:
+    U1 = [u_sol(1:3:end,1), u_sol(2:3:end,1), u_sol(3:3:end,1)];
+    clf
+    trisurf(tetr,p(:,1)+U1(:,1),p(:,2)+U1(:,2),p(:,3)+U1(:,3),U1(:,3));
+    axis equal,colorbar,title(['FEM solution at time = ' num2str(ts(t))])
+    view(-65 - 2*ts(t), 24);
+    saveas(fig, ['figures/time' num2str(ts(t)) '.png']);
+    
+    
 end
 
 %% plottting
@@ -234,7 +249,71 @@ writeVTF2(p, tetr, 'Displacement', U1, 'FileName', 'bridge_displacement.vtf');
 
 
 
-% wanting to find stresses:
+%% wanting to find stresses:
+% trying to build strain-displacement matrix:
+normedElementStress = zeros(length(tetr),1);
+for i = 1:length(tetr)
+    nodes = tetr(i,:);
+    P = p(nodes,:);       % Active points in tetrangle
+    
+    % Calculating area
+    Q = [[1;1;1;1], P];
+    %vol = abs(det(Q))/6;
+    
+    
+    % Getting stiffness matetrx
+    % Finding constants in phi (basis function = [1, x, y, z] * c)
+    c1 = Q\[1; 0; 0; 0];
+    c2 = Q\[0; 1; 0; 0];
+    c3 = Q\[0; 0; 1; 0];
+    c4 = Q\[0; 0; 0; 1];
+    %c = inv(Q);
+    c = [c1, c2, c3, c4];
+    
+    % B: strain displacement matrix
+    B = zeros(6,12);
+    B(1,1:3:end) = c(2,:);
+    B(2,2:3:end) = c(3,:);
+    B(3,3:3:end) = c(4,:);
+    
+    B(4,1:3:end) = c(3,:);
+    B(4,2:3:end) = c(2,:);
+    
+    B(5,2:3:end) = c(4,:);
+    B(5,3:3:end) = c(3,:);
+    
+    B(6,1:3:end) = c(4,:);
+    B(6,3:3:end) = c(2,:);
+    
+    
+    % u_e: displacement field:
+    map(1:3:3*length(nodes)) = 3*nodes-2;
+    map(2:3:3*length(nodes)) = 3*nodes-1;
+    map(3:3:3*length(nodes)) = 3*nodes;
+    
+    u_e = u_sol(map)';
+    
+    % simga: stress vector for element:
+    
+    sigma = C*B*u_e;
+    normedElementStress(i) = normedElementStress(i) + norm(sigma,2);
+    
+end
+
+%% Averaging out stresses for each node:
+normedNodeStress = zeros(N,1);
+for i = 1:N
+    [lines, cols] = find(tetr == i);
+    normedNodeStress(i) = mean(normedElementStress(lines));   
+end
+
+
+
+% Plotting with stress:
+figure
+%subplot(1,2,1)
+trisurf(tetr,p(:,1)+U1(:,1),p(:,2)+U1(:,2),p(:,3)+U1(:,3),normedNodeStress);
+view(3),axis equal,colorbar,title('FEM solution')
 
 
 
